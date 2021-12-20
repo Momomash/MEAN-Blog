bc9c131c0295ce796d919ec095e90917e9a7c8fb
Momomash <momomash110697@gmail.com>
2021-12-20 15:23:07 +0300

    add User model

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
-router.get("/reg", (req, res) => {
-  res.send("Registration Page");
+router.post("/reg", (req, res) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    login: req.body.login,
+    password: req.body.password,
+  });
+  User.addUser(newUser, (err) => {
+    err
+      ? res.json({ success: false, message: "User has not been added." })
+      : res.json({ success: false, message: "User has been added." });
+  });
 });
 
 router.get("/auth", (req, res) => {
