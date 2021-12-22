c0ae7469e33298438d1b6dd24fc2548cb5d45aa6
Momomash <momomash110697@gmail.com>
2021-12-22 15:54:40 +0300

    add reg form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { RegComponent } from './reg/reg.component';
-import { AuthComponent } from './auth/auth.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+import { RegComponent } from "./reg/reg.component";
+import { AuthComponent } from "./auth/auth.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
@@ -16,13 +17,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeaderComponent,
     RegComponent,
     AuthComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/reg/reg.component.ts
+++ frontend/src/app/reg/reg.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reg',
   templateUrl: './reg.component.html',
-  styleUrls: ['./reg.component.scss']
+  styleUrls: ['./reg.component.scss'],
 })
-export class RegComponent implements OnInit {
+export class RegComponent {
+  regForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    login: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get controls() {
+    return this.regForm.controls as { [key: string]: AbstractControl; };
   }
 
+  onSubmit() {
+    console.log(this.regForm.value);
+  }
 }
